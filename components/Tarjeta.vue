34ada8d4ca2f46a9a4c42da50d5424220eba6a35
Victor Zabala <pasante.universitario@interedes.com.co>
2021-01-25 16:03:05 -0500

    avances en login

---
+++ components/Tarjeta.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-card
+      :title="item.productName"
+      :img-src="item.productImage"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem"
+      class="mb-2"
+    >
+      <b-card-text>
+        <h4>Price: {{ item.productPrice }}</h4>
+        <p>{{ item.productDescription }}</p>
+      </b-card-text>
+      <div class="row">
+        <div class="col-6">
+          <b-button @click="anadirProducto(item)" variant="success"
+            >Añadir</b-button
+          >
+        </div>
+        <div class="col-6">
+          <b-button @click="quitarProducto(item)" variant="danger">Quitar</b-button>
+        </div>
+      </div>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    item: { type: Object },
+  },
+  data: () => ({}),
+  methods: {
+    anadirProducto(producto) {
+        this.$emit('producto', producto)
+    },
+    quitarProducto(producto) {
+        this.$emit('quitarProducto', producto)
+    },
+  },
+  mounted() {},
+}
+</script>
